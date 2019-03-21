0cd4a50e650abc003ac287af8ff02599b74734cc
Sricharan Sanakkayala <sanakkayala12@gmail.com>
2019-03-20 21:17:58 -0500

    3/20 added filters

--- Picture Lab/src/Picture.java
+++ Picture Lab/src/Picture.java
@@ -139,6 +139,36 @@ public class Picture extends SimplePicture
       }
     }
   }
+  /** Method to make grayscale colors*/
+  public void crazyFun()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+    	  if(pixelObj.getRed()%2==0) {
+     		 pixelObj.setRed(0);
+     	}else {
+     		 pixelObj.setRed(255);
+     	}
+     	if(pixelObj.getGreen()%2==0) {
+    		 	pixelObj.setGreen(0);
+     	}else {
+    		 	pixelObj.setGreen(255);
+     	}
+     	if(pixelObj.getBlue()%2==0) {
+     		pixelObj.setBlue(0);
+     	}else {
+    		 	pixelObj.setBlue(255);
+     	}
+//    	int color = (pixelObj.getRed()+pixelObj.getGreen()+pixelObj.getBlue())/3;
+//        pixelObj.setRed(color);
+//        pixelObj.setGreen(color);
+//        pixelObj.setBlue(color);
+      }
+    }
+  }
   
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
@@ -159,6 +189,46 @@ public class Picture extends SimplePicture
       }
     } 
   }
+  /** Method that mirrors the picture around a 
+   * vertical mirror in the center of the picture
+   * from right to left */
+ public void mirrorVerticalRightToLeft()
+ {
+   Pixel[][] pixels = this.getPixels2D();
+   Pixel leftPixel = null;
+   Pixel rightPixel = null;
+   int width = pixels[0].length;
+   for (int row = 0; row < pixels.length; row++)
+   {
+     for (int col = 0; col < width / 2; col++)
+     {
+       leftPixel = pixels[row][col];
+       rightPixel = pixels[row][width - 1 - col];
+       leftPixel.setColor(rightPixel.getColor());
+     }
+   } 
+ }
+ /** Method that mirrors the picture around a 
+  * vertical mirror in the center of the picture
+  * from right to left */
+public void mirrorHorizontal()
+{
+  Pixel[][] pixels = this.getPixels2D();
+  Pixel upPixel = null;
+  Pixel downPixel = null;
+  int width = pixels[0].length;
+  int height = pixels.length;
+  
+    for (int col = 0; col < width; col++)
+    {
+    	for (int row = 0; row < height/2; row++)
+    	  {
+      upPixel = pixels[row][col];
+      downPixel = pixels[row][width - 1 - col];
+      downPixel.setColor(upPixel.getColor());
+    }
+  } 
+}
   
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
@@ -267,7 +337,7 @@ public class Picture extends SimplePicture
   {
     Picture beach = new Picture("beach.jpg");
     beach.explore();
-    beach.zeroBlue();
+    beach.mirrorVerticalRightToLeft();
     beach.explore();
   }
   
